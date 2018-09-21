a5c14f4d6e4d5c6b49e00d5c869d87194d62ebc3
JayGarxP <x>
2018-09-21 16:25:31 -0700

    changes and adds are seperate commits????

--- SwordClash/Seth Le Podeu/Assets/SWORDCLASH/Scripts/Camera/ResponsiveCamera.cs
+++ SwordClash/Seth Le Podeu/Assets/SWORDCLASH/Scripts/Camera/ResponsiveCamera.cs
@@ -2,15 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+/*
+ * https://gist.github.com/ditzel/d3dde9d60df08e714f9c571db81f3937#file-cameraaspectratioscaler-cs
+ * Source code from indie dev youtuber ditzel AKA DitzelGames ~Aug 2016
+ * Actaully SUCKS! Cannot use it...
+ */
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+public class ResponsiveCamera : MonoBehaviour
+{
+  
+    //EDITOR_FIELDS
+    public Camera defaultStartCamera;
+    public Camera PortraitCamera;
+    public Camera LandscapeCamera;
+
+    private Camera _activeCamera;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //defaultStartCamera = GetComponent<Camera>();
+        _activeCamera = defaultStartCamera;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkScreenOrientation();
+
+    }
+
+    void SetActiveCamera(Camera newActiveCamera)
+    {
+
+        _activeCamera.enabled = false;
+        _activeCamera = newActiveCamera;
+        _activeCamera.enabled = true;
+        
+        //Only needed for 3D sound.
+        //_activeCamera.GetComponent<AudioListener>().enabled = true;
+
+
+    }
+
+    void checkScreenOrientation()
+    {
+        // Instead of polling each frame, consider using an event system.
+        // There are UnityEvents and Input.ScreenOrientation would also be MUCH better.
+        if (Screen.width <= Screen.height)
+        {
+            SetActiveCamera(PortraitCamera);
+        }
+        else
+        {
+            SetActiveCamera(LandscapeCamera);
+        }
+    }
+
+
+    
 }
--- SwordClash/Seth Le Podeu/Assets/SWORDCLASH/Scripts/PlayerController.cs
+++ SwordClash/Seth Le Podeu/Assets/SWORDCLASH/Scripts/PlayerController.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    #region EDITOR_FIELDS
+    
+    #endregion
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+       
+
+    }
+
+    ////FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    //void FixedUpdate()
+    //{
+        
+    //}
+
+    void Update()
+    {
+        
+
+
+    }
+
+
+    ////OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+    //    if (other.gameObject.CompareTag("PickUp"))
+    //    {
+    //        other.gameObject.SetActive(false);
+    //        count += 1;
+    //        //TODO: Can I group UI elements together, score being Count and a num value tied to actual score?
+    //        countText.text = HelperClassTest.UpdateTextField("Count: ", count);
+
+    //        if (count >= 2)
+    //            //... then set the text property of our winText object to "You win!"
+    //            winText.text = "You win YEET!";
+    //    }
+    //}
 }
