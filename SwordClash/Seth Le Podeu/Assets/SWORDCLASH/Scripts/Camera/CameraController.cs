de15829946ce8ec61405c551ea7a3b5ef8ab5633
JayGarxP <x>
2018-09-22 13:57:12 -0700

    Observer event implemented for device orientation; polls each half-second to switch camera if needed.

---
+++ SwordClash/Seth Le Podeu/Assets/SWORDCLASH/Scripts/Camera/CameraController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //EDITOR_FIELDS
+    public Camera defaultStartCamera;
+    public Camera PortraitCamera;
+    public Camera LandscapeCamera;
+
+    private Camera _activeCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        _activeCamera = defaultStartCamera;
+        DeviceChange.OnOrientationChange += MyOrientationChangeCode;
+        //DeviceChange.OnResolutionChange += MyResolutionChangeCode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    void MyOrientationChangeCode(DeviceOrientation orientation)
+    {
+
+        switch (orientation)
+        {
+            case DeviceOrientation.LandscapeLeft:
+                SetActiveCamera(LandscapeCamera);
+                break;
+            case DeviceOrientation.LandscapeRight:
+                SetActiveCamera(LandscapeCamera);
+                break;
+            case DeviceOrientation.Portrait:
+                SetActiveCamera(PortraitCamera);
+                break;
+            case DeviceOrientation.PortraitUpsideDown:
+                SetActiveCamera(PortraitCamera);
+                break;
+
+            default:
+                SetActiveCamera(defaultStartCamera);
+                break;
+        }
+
+
+
+    }
+
+    void MyResolutionChangeCode(Vector2 resolution)
+    {
+    }
+
+    void SetActiveCamera(Camera newActiveCamera)
+    {
+        if (newActiveCamera != _activeCamera)
+        {
+            _activeCamera.enabled = false;
+            _activeCamera = newActiveCamera;
+            _activeCamera.enabled = true;
+        }
+    }
+
+}
