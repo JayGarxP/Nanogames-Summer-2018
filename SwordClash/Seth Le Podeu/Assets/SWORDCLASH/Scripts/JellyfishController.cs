0455ab73139f459e61fafe0be0b26f830de23f61
JayGarxP <x>
2018-11-25 14:53:57 -0800

    Static OnTrigger2D collider with jellyfish working

---
+++ SwordClash/Seth Le Podeu/Assets/SWORDCLASH/Scripts/JellyfishController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordClash
+{
+    public class JellyfishController : MonoBehaviour
+    {
+        //OnCollisionEnter() is event Publisher for trigger collision (hit jellyfish); Subscriber to collision event is tentacleController (where state machine will be implemented)
+        public event EventHandler JellyfishHitByTentacleTip_Event;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+     
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            //Raise collide event, subscribed to in TentacleController
+            JellyfishHitByTentacleTip_Event(this, EventArgs.Empty);
+
+            ////the subscriber class needs a reference to the publisher class in order to subscribe to its events.
+            //void HandleCustomEvent(object sender, CustomEventArgs a)
+            //{
+            //    // Do something useful here.  
+            //}
+
+            //Subscripe +=; Unsub -=;
+            //publisher.RaiseCustomEvent += HandleCustomEvent;  
+            //  publisher.RaiseCustomEvent -= HandleCustomEvent; 
+
+        }
+
+
+
+
+
+    }
+}
